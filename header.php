8f02d2edf6c687f74e33ae18fce9769a4c91680d
Lennon Santos <lennonsbueno@gmail.com>
2016-07-23 17:07:46 -0300

    Primeira versão simples

---
+++ header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title><?php wp_title(); ?></title>
+	<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
+</head>
+<body>
+	<header>
+		<h1><?php bloginfo('name'); ?></h1>
+		<nav>
+		</nav>
+	</header>
